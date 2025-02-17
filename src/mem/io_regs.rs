cf51b5b865afcca5588c09dc7e1d1851ee56af6a
Matthew Maurice Ducasse <matthewmducasse@gmail.com>
2025-02-16 20:00:17 -0800

    Moved io_reg_data into io_regs.rs.

--- src/mem/io_regs.rs
+++ src/mem/io_regs.rs
@@ -1,12 +1,13 @@
 use std::collections::HashMap;
 
+use io_reg_data::IoRegData;
 use num::FromPrimitive;
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 
 use crate::{debug, util::math::set_bits8_masked};
 
-use super::{array::Array, io_reg_data::IoRegData, sections::MemSection, Addr};
+use super::{array::Array, sections::MemSection, Addr};
 
 #[derive(Hash, Clone, Copy, PartialEq, Eq, Debug, FromPrimitive, EnumIter)]
 pub enum IoReg {
@@ -172,5 +173,76 @@ impl IoRegs {
         f(data);
 
         return *data;
+    }
+}
+
+mod io_reg_data {
+    use super::IoReg;
+
+    /// Describes special behavior for a given IO register.
+    pub struct IoRegData {
+        reg: IoReg,
+        read_mask: u8,
+        write_mask: u8,
+    }
+
+    impl IoRegData {
+        fn new(reg: IoReg) -> Self {
+            Self {
+                reg,
+                read_mask: 0xFF,
+                write_mask: 0xFF,
+            }
+        }
+
+        pub fn read_mask(&self) -> u8 {
+            self.read_mask
+        }
+
+        pub fn write_mask(&self) -> u8 {
+            self.write_mask
+        }
+
+        fn with_read_mask(mut self, read_mask: u8) -> Self {
+            self.read_mask = read_mask;
+            self
+        }
+
+        fn with_write_mask(mut self, write_mask: u8) -> Self {
+            self.write_mask = write_mask;
+            self
+        }
+
+        pub fn from_reg(reg: IoReg) -> Self {
+            let data = Self::new(reg);
+
+            match reg {
+                IoReg::P1 => data.with_write_mask(0b1111_0000),
+                IoReg::Sb => data,
+                IoReg::Sc => data,
+                IoReg::Div => data,
+                IoReg::Tima => data,
+                IoReg::Tma => data,
+                IoReg::Tac => data,
+                IoReg::If => data
+                    .with_read_mask(0b0001_1111)
+                    .with_write_mask(0b0001_1111),
+                // todo: Sound regs...
+                IoReg::Lcdc => data,
+                IoReg::Stat => data.with_write_mask(0b1111_1000),
+                IoReg::Scy => data,
+                IoReg::Scx => data,
+                IoReg::Ly => data.with_write_mask(0b0000_0000),
+                IoReg::Lyc => data,
+                IoReg::Dma => data,
+                IoReg::Bgp => data,
+                IoReg::Obp0 => data,
+                IoReg::Obp1 => data,
+                IoReg::Wy => data,
+                IoReg::Wx => data,
+                // todo: CGB regs...
+                IoReg::Ie => data,
+            }
+        }
     }
 }
--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -1,6 +1,5 @@
 mod addr;
 mod array;
-mod io_reg_data;
 pub mod io_regs;
 pub mod mem;
 pub mod sections;
